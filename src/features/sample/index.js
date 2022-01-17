40d3b115444779a33d49883e10c58aa4dc98c916
HoHuy09 <huyhtph07087@fpt.edu.vn>
2022-01-17 19:25:09 +0700

    màn hình ListQuiz
    as

--- src/features/sample/index.js
+++ src/features/sample/index.js
@@ -1,24 +1,141 @@
+
 import React from "react";
-import { Image } from "antd";
+import { Col, Input, Row, Skeleton } from "antd";
 
 import { Link } from "react-router-dom";
 import MainLayout from "layouts/main.layout";
-import logo from "../../assets/logo.png"; // Tell webpack this JS file uses this image
+import {FaListUl,FaAngleDown,FaCaretRight,FaAppleAlt,FaPencilAlt,FaTrash,FaChartBar} from "react-icons/fa"
 
 import "./styles.scss";
 
-const quizzes = [{ _id: 123 }];
-
-const Sample = () => {
+const ListQuizz = ({ socket }) => {
   return (
     <MainLayout>
-      <div className="sample">
-        <div className="enter-pin">
-          <Image width={300} src={logo} />
-        </div>
+      <br></br>
+      <div className="listquiz">
+        <Row>
+          <Col span={16} offset={4}>
+            <div className="search">
+              <Input size="large" placeholder="Search" />
+            </div>
+            <br></br>
+            <div className="header">
+              <Row justify="space-between">
+                <Col span={4}>
+                  <FaListUl/>
+                  <span id="span1" >3 Quiz</span>
+                </Col>
+                <Col span={4}>
+                  <button  id="filter" type="" className="btn">
+                    <FaAngleDown/> Mới nhất trước 
+                  </button>
+                </Col>
+              </Row>
+            </div>
+            <br></br>
+            <div className="quiz">
+              <Row>
+                <Col>
+                  <h2>
+                    Poly quiz! for formative assessment formative assessment
+                  </h2>
+                </Col>
+              </Row>
+              <Row>
+                <Col id="fa">
+                  <FaCaretRight/>  <span>10 lần chơi</span>
+                </Col>
+                <Col id="fa"> 
+                <FaListUl/>    <span>  3 câu hỏi</span>
+                </Col>
+              </Row>
+              <Row className="quiz-button">
+                <Col>
+                  <button type="button" className="starr"> <FaAppleAlt/> Bắt đầu kiểm tra</button>
+                </Col>
+                <Col green-8>
+                  <button type="button" className="edit"> <FaPencilAlt/> Chỉnh sửa</button>
+                </Col>
+                <Col green-8>
+                  <button type="button" className="delete"> <FaTrash/> Xóa</button>
+                </Col>
+                <Col green-8>
+                  <button type="button" className="report"> <FaChartBar/> Báo cáo</button>
+                </Col>
+              </Row>
+              <br></br>
+            </div>
+            <br></br>
+            <div className="quiz">
+              <Row>
+                <Col>
+                  <h2>
+                    Poly quiz! for formative assessment formative assessment
+                  </h2>
+                </Col>
+              </Row>
+              <Row>
+                <Col id="fa">
+                  <FaCaretRight/>  <span>10 lần chơi</span>
+                </Col>
+                <Col id="fa"> 
+                <FaListUl/>    <span>  3 câu hỏi</span>
+                </Col>
+              </Row>
+              <Row className="quiz-button">
+                <Col>
+                  <button type="button" className="starr"> <FaAppleAlt/> Bắt đầu kiểm tra</button>
+                </Col>
+                <Col green-8>
+                  <button type="button" className="edit"> <FaPencilAlt/> Chỉnh sửa</button>
+                </Col>
+                <Col green-8>
+                  <button type="button" className="delete"> <FaTrash/> Xóa</button>
+                </Col>
+                <Col green-8>
+                  <button type="button" className="report"> <FaChartBar/> Báo cáo</button>
+                </Col>
+              </Row>
+              <br></br>
+            </div>
+            <br></br>
+            <div className="quiz">
+              <Row>
+                <Col>
+                  <h2>
+                    Poly quiz! for formative assessment formative assessment
+                  </h2>
+                </Col>
+              </Row>
+              <Row>
+                <Col id="fa">
+                  <FaCaretRight/>  <span>10 lần chơi</span>
+                </Col>
+                <Col id="fa"> 
+                <FaListUl/>    <span>  3 câu hỏi</span>
+                </Col>
+              </Row>
+              <Row className="quiz-button">
+                <Col>
+                  <button type="button" className="starr"> <FaAppleAlt/> Bắt đầu kiểm tra</button>
+                </Col>
+                <Col green-8>
+                  <button type="button" className="edit"> <FaPencilAlt/> Chỉnh sửa</button>
+                </Col>
+                <Col green-8>
+                  <button type="button" className="delete"> <FaTrash/> Xóa</button>
+                </Col>
+                <Col green-8>
+                  <button type="button" className="report"> <FaChartBar/> Báo cáo</button>
+                </Col>
+              </Row>
+              <br></br>
+            </div>
+          </Col>
+        </Row>
       </div>
     </MainLayout>
   );
 };
 
-export default Sample;
+export default ListQuizz;
