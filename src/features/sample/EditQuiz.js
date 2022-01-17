9f64157cbdc7888d4e35d8ac0d81143b797353ae
tbmfg <fury.ghost.0001@gmail.com>
2022-01-17 21:53:42 +0700

    comment explain code

--- src/features/sample/EditQuiz.js
+++ src/features/sample/EditQuiz.js
@@ -7,7 +7,7 @@ const initialQuestions = [
   {
     question: "Câu hỏi 1",
     answers: ["Wrong Answer", "Correct Answer", "Wrong Answer", "Wrong Answer"],
-    correctAnswer: 1,
+    correctAnswer: 0,
     time: 30,
   },
   {
@@ -25,18 +25,28 @@ const initialQuestions = [
   },
 ];
 
+const state = {
+  questions: initialQuestions,
+  title: "Title",
+};
+
 const EditQuiz = () => {
   const [questions, setQuestions] = React.useState(initialQuestions);
 
   const duplicateQuestion = (index) => {
+    // Tìm đến câu hỏi cần nhân bản theo index
     const questionToDuplicate = questions.find((qt, i) => index === i);
     let newQuestions = [...questions];
+    // Chèn câu hỏi cần nhân bản vào mảng questions
     newQuestions.splice(index, 0, questionToDuplicate);
+
     setQuestions(newQuestions);
   };
 
   const deleteQuestion = (index) => {
+    // Nếu chỉ còn lại 1 câu hỏi thì ko làm gì
     if (questions.length === 1) return;
+    // Lọc ra các câu hỏi không có index trùng với index cần xoá, (index - thứ thự trong array)
     const newQuestions = questions.filter((qt, i) => index !== i);
     setQuestions(newQuestions);
   };
@@ -44,13 +54,17 @@ const EditQuiz = () => {
   return (
     <div className="sample bg-gray-edit">
       <h3>Poly quiz! for formative assessment</h3>
+      {/* Map câu hỏi ra giao diện */}
       {questions.map((qt, index) => (
         <div className="bg-white-form" key={index}>
           <form className="form-edit">
+            {/* Câu hỏi */}
             <label>{qt.question}</label>
             <div className="form-padding">
+              {/* Map câu trả lời ra giao diện */}
               {qt.answers.map((answer, i) => (
                 <div className="answer" key={i}>
+                  {/* Kiểm tra điều hiện có phải câu trả lời đúng */}
                   {qt.correctAnswer === i ? (
                     <div className="answer__box-success" />
                   ) : (
@@ -62,14 +76,17 @@ const EditQuiz = () => {
             </div>
             <div className="btn">
               <Button>
+                {/* Hiển thị thời gian câu hỏi */}
                 <i class="far fa-clock"></i> {qt.time} giây
               </Button>
+              {/* Nút Nhân bản câu hỏi */}
               <Button
                 className="btn-mx"
                 onClick={() => duplicateQuestion(index)}
               >
                 <i class="far fa-copy"></i> Nhân bản
               </Button>
+              {/* Nút Xoá câu hỏi */}
               <Button onClick={() => deleteQuestion(index)}>
                 <i class="far fa-trash-alt"></i> Xóa
               </Button>
