91631a88cea5996b41495bf7aa9a51ab73595052
laquangduc <59763734+laquangduc@users.noreply.github.com>
2022-01-21 23:21:49 +0700

    edit-l2

--- src/features/sample/EditQuiz.js
+++ src/features/sample/EditQuiz.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Card } from "antd";
 
 import { Link } from "react-router-dom";
@@ -6,86 +6,84 @@ import MainLayout from "layouts/main.layout";
 import { Row, Col } from "antd";
 import "./styles.scss";
 
-const quizzes = [{ _id: 123 }];
-
+const quizzes = [
+  {
+    name: "cau hoi 1",
+    answer: ["chon 1", "chon 2", "chon 3", "chon 4"],
+    time: 200,
+    correct: 1,
+  },
+  {
+    name: "cau hoi 2",
+    answer: ["chon 1", "chon 2", "chon 3", "chon 4"],
+    time: 200,
+    correct: 2,
+  },
+  {
+    name: "cau hoi 3",
+    answer: ["chon 1", "chon 2", "chon 3", "chon 4"],
+    time: 200,
+    correct: 1,
+  },
+  {
+    name: "cau hoi 4",
+    answer: ["chon 1", "chon 2", "chon 3", "chon 4"],
+    time: 200,
+    correct: 1,
+  },
+];
 const EditQuiz = () => {
+  const [state, setState] = useState(quizzes);
+  const deleteQuiz = (abc) => {
+    const newQuiz = state.filter((quiz,i)=> abc !== i)
+    setState(newQuiz)
+  }
+
+  const double = (abc)=>{
+    const newQuiz = state.find((item,index) => index===abc);
+
+    const newState = [...state];
+    newState.splice(abc,0,newQuiz);
+    setState(newState)
+  }
+
   return (
     <div className="sample bg-gray-edit">
       <h3>Poly quiz! for formative assessment</h3>
-      <div className="bg-white-form ">
-        <form className="form-edit">
-          <label>
-            Replace this with a simple question. Use the language and vocabulary
-            that you know will be used in the unit test ?
-          </label>
-          <div className="form-padding">
-            <div className="answer">
-              <div className="answer__box-wrong" />
-              <div className="answer-px">Wrong Answer</div>
-            </div>
-            <div className="answer">
-              <div className="answer__box-success" />
-              <div className="answer-px">Correct Answer</div>
-            </div>
-            <div className="answer">
-              <div className="answer__box-wrong" />
-              <div className="answer-px">Wrong Answer</div>
-            </div>
-            <div className="answer">
-              <div className="answer__box-wrong" />
-              <div className="answer-px">Wrong Answer</div>
-            </div>
-          </div>
-          <div className="btn">
-            <button>
-              <i class="far fa-clock"></i> 30 giây
-            </button>
-            <button className="btn-mx">
-              <i class="far fa-copy"></i> Nhân bản
-            </button>
-            <button>
-              <i class="far fa-trash-alt"></i> Xóa
-            </button>
-          </div>
-        </form>
-      </div>
-      <div className="bg-white-form ">
-        <form className="form-edit">
-          <label>
-            Replace this with a simple question. Use the language and vocabulary
-            that you know will be used in the unit test ?
-          </label>
-          <div className="form-padding">
-            <div className="answer">
-              <div className="answer__box-wrong" />
-              <div className="answer-px">Wrong Answer</div>
-            </div>
-            <div className="answer">
-              <div className="answer__box-success" />
-              <div className="answer-px">Correct Answer</div>
-            </div>
-            <div className="answer">
-              <div className="answer__box-wrong" />
-              <div className="answer-px">Wrong Answer</div>
-            </div>
-            <div className="answer">
-              <div className="answer__box-wrong" />
-              <div className="answer-px">Wrong Answer</div>
-            </div>
-          </div>
-          <div className="btn">
-            <button>
-              <i class="far fa-clock"></i> 30 giây
-            </button>
-            <button className="btn-mx">
-              <i class="far fa-copy"></i> Nhân bản
-            </button>
-            <button>
-              <i class="far fa-trash-alt"></i> Xóa
-            </button>
-          </div>
-        </form>
-      </div>
+      {state
+        ? state.map((question, index) => {
+            return (
+              <div className="bg-white-form " key={index}>
+                <form className="form-edit">
+                  <label>{question.name}</label>
+                  <div className="form-padding">
+                    {question.answer.map((as, index) => {
+                      return (
+                        <div className="answer"> 
+                            {question.correct == index ?   <div className="answer__box-success" /> 
+                            :    <div className="answer__box-wrong" /> }                                   
+                            <div className="answer-px">{as}</div>                       
+                        </div>
+                      );
+                    })}
+       
+                  </div>
+                  <div className="btn">
+                    <button>
+                      <i class="far fa-clock"></i> {question.time} giây
+                    </button>
+                    <button type="buttom" className="btn-mx" onClick={() =>double(index)}>
+                      <i class="far fa-copy"></i> Nhân bản
+                    </button>
+                    <button onClick={() =>deleteQuiz(index)}>
+                      <i class="far fa-trash-alt"></i> Xóa
+                    </button>
+                  </div>
+                </form>
+              </div>
+            );
+          })
+        : null}
       <div className="add-answer">
         <button>
           <i class="fas fa-plus"></i>Thêm câu hỏi
