0fc1d260f689009261b93d8da9db124cdb9b407e
HoHuy09 <huyhtph07087@fpt.edu.vn>
2022-01-25 16:51:37 +0700

    man setting

--- src/features/settingame/index.js
+++ src/features/settingame/index.js
@@ -1,56 +1,100 @@
 import React from "react";
-import { Col, Row,Button} from "antd";
+import { Col, Row, Button, Input, Image, Select,Radio} from "antd";
 
-import { Link } from "react-router-dom";
 import MainLayout from "layouts/main.layout";
 
 import "./styles.scss";
 
-const ShowSetingame = () => {
-  constSetting = () =>{
-      
-  }
- 
+const Settingame = () => {
+  const { TextArea } = Input;
+  const { Option } = Select;
+  const Setting = () => {};
+
   return (
     <MainLayout>
       <br></br>
-      <Button onClick={()=>Setting()}></Button>
+      <Button onClick={() => Setting()}></Button>
       <div className="setting">
         <Row>
           <Col span={16} offset={4}>
-                <div className="test">
-                    <h2>Back ground</h2>
-                    <select id="sl-box">
-                        <option>
-                            ádasjdklasjdl
-                        </option>
-                        <option>
-                            test
-                        </option>
-                    </select>
+            <div className="setting">
+              <h2>CÀI ĐẶT</h2>
+              <br></br>
+              <div className="box-setting">
+                <div className="left">
+                  <div className="box">
+                    <h3>Tiêu đề</h3>
+                    <Input
+                      id="ip-left"
+                      size="large"
+                      placeholder="Nhập tiêu đề . . . "
+                    />
+                  </div>
+                  <div className="box">
+                    <h3>Mô tả</h3>
+                    <TextArea
+                      id="ip-left"
+                      rows={4}
+                      placeholder="Nhập mô tả . . . "
+                    />
+                  </div>
+                  <div className="box">
+                    <h3>Lưu</h3>
+                    <Select
+                      defaultValue="My PolyQuizz"
+                      style={{ width: 400 }}
+                      
+                    >
+                      <Option value="jack">Jack</Option>
+                      <Option value="lucy">Lucy</Option>
+                      <Option value="disabled" disabled>
+                        Disabled
+                      </Option>
+                      <Option value="Yiminghe">yiminghe</Option>
+                    </Select>
+                    
+                  </div>
+                  <div id="btn-cancle">
+                        <Button>Cancle</Button>
+                  </div>
                 </div>
-                <div className="test">
-                    <h2>Nhạc nền</h2>
-                    <select id="sl-box">
-                        <option>
-                            ádasjdklasjdl
-                        </option>
-                        <option>
-                            test
-                        </option>
-                    </select>
+                <div className="right">
+                  <div className="box">
+                    <h3>Background</h3>
+                    <Image
+                      width={400}
+                      height={200}
+                      src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                    />
+                    <Button id="btn">Chọn</Button>
+                  </div>
+                  <div className="box">
+                    <h3>Nhạc nền</h3>
+                    <Select
+                      defaultValue="My PolyQuizz"
+                      style={{ width: 400 }}
+                      
+                    >
+                      <Option value="jack">Jack</Option>
+                      <Option value="lucy">Lucy</Option>
+                      <Option value="disabled" disabled>
+                        Disabled
+                      </Option>
+                      <Option value="Yiminghe">yiminghe</Option>
+                    </Select>
+                    
+                  </div>
+                  <div className="box">
+                      <h3>HIện thị</h3>
+                      <Radio>Cộng cộng</Radio>
+                      <Radio>Riêng tư</Radio>
+                  </div>
+                  <div id="btn-done">
+                        <Button>Done</Button>
+                  </div>
                 </div>
-                <div className="test">
-                    <h2>logo</h2>
-                    <select id="sl-box">
-                        <option>
-                            ádasjdklasjdl
-                        </option>
-                        <option>
-                            test
-                        </option>
-                    </select>
-                </div>
+              </div>
+            </div>
           </Col>
         </Row>
       </div>
@@ -58,4 +102,4 @@ const ShowSetingame = () => {
   );
 };
 
-export default ShowSetingame;
+export default Settingame;
