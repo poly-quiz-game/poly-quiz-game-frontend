f5c20d91d4cf4805c3a12811f4e392a7e75dec87
HoHuy09 <huyhtph07087@fpt.edu.vn>
2022-02-13 11:53:17 +0700

    mm

--- src/features/settingame/index.js
+++ src/features/settingame/index.js
@@ -1,70 +1,67 @@
-import React from "react";
-import { Form, Input, Button,Select } from 'antd';
-
+import React, { useState } from "react";
+import { Form, Input, Button, Select } from "antd";
 import MainLayout from "layouts/main.layout";
 
 import "./styles.scss";
 
 const Settingame = () => {
-  
   const onFinish = (values) => {
-    console.log('Success:', values);
+    console.log("Success:", values);
   };
-  
+
   const onFinishFailed = (errorInfo) => {
-    console.log('Failed:', errorInfo);
+    console.log("Failed:", errorInfo);
   };
 
   return (
     <MainLayout>
-      <br></br><br></br><br></br>
-    <Form
-      name="basic"
-      labelCol={{
-        span: 6,
-      }}
-      wrapperCol={{
-        span: 12,
-      }}
-      initialValues={{
-        remember: true,
-      }}
-      onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
-      autoComplete="off"
-    >
-      <Form.Item
-        label="Tên Quizz"
-        name="title"
-        rules={[
-          {
-            required: true,
-            message: 'Mời nhập tên quizz!',
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item name="type" label="Chế độ" rules={[{ required: true }]}>
-        <Select
-          placeholder="Chọn chế độ"
-          allowClear
-        >
-          <Option value="public">Công khai</Option>
-          <Option value="private">Riêng tư</Option>
-        </Select>
-      </Form.Item>
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
+      <br></br>
+      <br></br>
+      <br></br>
+      <Form
+        name="basic"
+        labelCol={{
+          span: 6,
         }}
+        wrapperCol={{
+          span: 12,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
       >
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        <Form.Item
+          label="Tên Quizz"
+          name="title"
+          rules={[
+            {
+              required: true,
+              message: "Mời nhập tên quizz!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item name="type" label="Chế độ" rules={[{ required: true }]}>
+          <Select placeholder="Chọn chế độ" allowClear>
+            <Option value="public">Công khai</Option>
+            <Option value="private">Riêng tư</Option>
+          </Select>
+        </Form.Item>
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
     </MainLayout>
   );
 };
