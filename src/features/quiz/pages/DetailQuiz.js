f482ff3315dd035c23390083b3caa00a0718fbb5
HoHuy09 <huyhtph07087@fpt.edu.vn>
2022-02-14 20:57:32 +0700

    commit man detail

--- src/features/quiz/pages/DetailQuiz.js
+++ src/features/quiz/pages/DetailQuiz.js
@@ -1,8 +1,31 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-import { Skeleton, Button, Card, List, Avatar } from "antd";
-
+import {
+  Skeleton,
+  Button,
+  Card,
+  List,
+  Avatar,
+  Layout,
+  Menu,
+  Breadcrumb,
+  Image,
+} from "antd";
+import {
+  UserOutlined,
+  LaptopOutlined,
+  NotificationOutlined,
+} from "@ant-design/icons";
+import {
+  FaListUl,
+  FaUserAlt,
+  FaCaretRight,
+  FaEllipsisV,
+  FaPencilAlt,
+  FaStar,
+  FaChartBar,
+} from "react-icons/fa";
 import { CheckCircleFilled, CloseCircleFilled } from "@ant-design/icons";
 
 import MainLayout from "layouts/main.layout";
@@ -11,6 +34,8 @@ import { Link } from "react-router-dom";
 import { fetchQuiz, selectQuiz, selectLoading } from "../quizSlice";
 
 import "./styles.css";
+const { SubMenu } = Menu;
+const { Header, Content, Sider } = Layout;
 
 const CorrectIcon = (
   <span style={{ color: "#52c41a" }}>
@@ -36,7 +61,7 @@ const DetailQuiz = ({ socket }) => {
 
   return (
     <MainLayout>
-      <div className="start-quiz">
+      {/* <div className="start-quiz">
         {loading ? (
           <Skeleton />
         ) : (
@@ -75,7 +100,95 @@ const DetailQuiz = ({ socket }) => {
             ))}
           </>
         )}
-      </div>
+      </div> */}
+      <Layout>
+        <Layout>
+          <Sider width={200} className="site-layout-background">
+            <Menu
+              mode="inline"
+              defaultSelectedKeys={["1"]}
+              defaultOpenKeys={["sub1"]}
+              style={{ height: "100%", borderRight: 0, width: "332px" }}
+            >
+              <div className="quiz-img">
+                <Image
+                  style={{
+                    width: "332px",
+                    height: "218px",
+                    paddingTop: "20px",
+                  }}
+                  src="https://dean2020.edu.vn/wp-content/uploads/2020/03/anh-cho-con.jpg"
+                />
+              </div>
+              <div className="title">
+                <h3>Tên Quizz: Animals</h3>
+              </div>
+              <div className="number-play">
+                <div className="content">
+                  <h4>1 lượt - 1 người chơi</h4>
+                </div>
+                <div className="button-icon">
+                  <div>
+                    <FaPencilAlt />
+                  </div>
+                  <div>
+                    <FaStar />
+                  </div>
+                  <div>
+                    <FaEllipsisV />
+                  </div>
+                </div>
+              </div>
+              <div className="button">
+                <div className="button1">
+                  <Button
+                    className="button-start"
+                    style={{ backgroundColor: "#416CDA" }}
+                  >
+                    <h3>Bắt đầu</h3>
+                  </Button>
+                </div>
+                <div className="button2">
+                  <Button
+                    className="button-atribute"
+                    style={{ backgroundColor: "#fff" }}
+                  >
+                    <h3>Thuộc tính</h3>
+                  </Button>
+                </div>
+              </div>
+              <div className="description">
+                <h5>
+                  Mô tả: <span>ccc</span>
+                </h5>
+              </div>
+              <div className="user">
+                <div>
+                  <FaUserAlt />
+                </div>
+                <div className="user-content">huyhtph07087@fpt.edu.vn</div>
+              </div>
+            </Menu>
+          </Sider>
+          <Layout style={{ padding: "0 24px 24px", paddingLeft: "150px" }}>
+            <div className="question">
+            <h3>Câu hỏi <span>(2)</span></h3>
+            </div>
+            <Content
+              className="site-layout-background"
+              style={{
+                padding: 24,
+                margin: 0,
+                minHeight: 927,
+              }}
+            >
+              <div className="quiz">aaaaaaaaaaaaa</div>
+              <div className="quiz">aaaaaaaaaaaaaaa</div>
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+      ,
     </MainLayout>
   );
 };
