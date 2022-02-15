baee13fe9a1572e046f851d7aaab9fcc1aed92ee
HoHuy09 <huyhtph07087@fpt.edu.vn>
2022-02-15 10:15:02 +0700

    update detailquizz

--- src/features/quiz/pages/DetailQuiz.js
+++ src/features/quiz/pages/DetailQuiz.js
@@ -18,7 +18,6 @@ import {
   NotificationOutlined,
 } from "@ant-design/icons";
 import {
-  FaListUl,
   FaUserAlt,
   FaCaretRight,
   FaEllipsisV,
@@ -121,7 +120,7 @@ const DetailQuiz = ({ socket }) => {
                 />
               </div>
               <div className="title">
-                <h3>Tên Quizz: Animals</h3>
+                <h2>Tên Quizz: {quiz.name}</h2>
               </div>
               <div className="number-play">
                 <div className="content">
@@ -164,18 +163,106 @@ const DetailQuiz = ({ socket }) => {
           </Sider>
           <Layout style={{ padding: "0 24px 24px", paddingLeft: "150px" }}>
             <div className="question">
-            <h3>Câu hỏi <span>(2)</span></h3>
+              <h3>
+                Câu hỏi <span>({quiz.questions.length})</span>
+              </h3>
             </div>
             <Content
               className="site-layout-background"
               style={{
                 padding: 24,
                 margin: 0,
-                minHeight: 927,
+                minHeight: 1000,
               }}
             >
-              <div className="quiz">aaaaaaaaaaaaa</div>
-              <div className="quiz">aaaaaaaaaaaaaaa</div>
+              {(quiz.questions || []).map((qt) => (<div className="quizquestion">
+                <div className="quizzquestion-top">
+                  <div className="quizquestion-left">
+                    <h4>1-Câu hỏi</h4>
+                    <h5>{qt.question}</h5>
+                  </div>
+                  <div className="quizquestion-right">
+                    <Image
+                      style={{
+                        width: "234px",
+                        height: "119px",
+                        paddingTop: "5px",
+                        borderRadius: "15px",
+                        marginRight: "20px",
+                        display: "block",
+                      }}
+                      src="https://dean2020.edu.vn/wp-content/uploads/2020/03/anh-cho-con.jpg"
+                    ></Image>
+                    <Button
+                    style={{
+                      position: "absolute",
+                      backgroundColor: "#151414",
+                      color: "#fff",
+                      borderRadius: "5px",
+                      opacity: "70%",
+                      border: 0,
+                      marginTop: '85px',
+                      marginLeft: '-80px'
+                    }}
+                  >
+                    20s
+                  </Button>
+                  </div>
+                </div>
+               
+                <div className="quizzquestion-bottom">
+                {(qt.answers || []).map((as) => (
+                  <div className="answer">
+                  <div className="answer-question">
+                    <div className="answer-question-left">
+                      <h3>A</h3>
+                    </div>
+                    <div className="answer-question-right">
+                      <h4>{as}</h4>
+                    </div>
+                  </div>
+                  <div className="answer-icon"><CheckCircleFilled/></div>
+                </div>
+                ))}
+                  
+                  {/* <div className="answer">
+                    <div className="answer-question">
+                      <div className="answer-question-left">
+                        <h3>B</h3>
+                      </div>
+                      <div className="answer-question-right">
+                        <h4>Con Mèo</h4>
+                      </div>
+                    </div>
+                    <div className="answer-icon"><CloseCircleFilled/></div>
+                  </div>
+                  <div className="answer">
+                    <div className="answer-question">
+                      <div className="answer-question-left">
+                        <h3>C</h3>
+                      </div>
+                      <div className="answer-question-right">
+                        <h4>Con Lợn</h4>
+                      </div>
+                    </div>
+                    <div className="answer-icon"><CloseCircleFilled/></div>
+                  </div>
+                  <div className="answer">
+                    <div className="answer-question">
+                      <div className="answer-question-left">
+                        <h3>D</h3>
+                      </div>
+                      <div className="answer-question-right">
+                        <h4>Con Gà</h4>
+                      </div>
+                    </div>
+                    <div className="answer-icon"><CloseCircleFilled/></div>
+                  </div> */}
+                </div>
+              </div>))}
+              
+              
+              
             </Content>
           </Layout>
         </Layout>
