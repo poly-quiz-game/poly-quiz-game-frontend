f5bd14edb090aa334760064c460b93b075bb5006
HoHuy09 <huyhtph07087@fpt.edu.vn>
2022-02-16 16:20:55 +0700

    update

--- src/features/quiz/pages/DetailQuiz.js
+++ src/features/quiz/pages/DetailQuiz.js
@@ -46,11 +46,11 @@ const IncorretIcocn = (
     <CloseCircleFilled />
   </span>
 );
-const QUESTION_LABELS = ['A','B','C','D']
+const QUESTION_LABELS = ["A", "B", "C", "D"];
 const DetailQuiz = ({ socket }) => {
   let params = useParams();
   const dispatch = useDispatch();
-  
+  const user = JSON.parse(localStorage.getItem("user"));
   const quiz = useSelector(selectQuiz);
   const loading = useSelector(selectLoading);
 
@@ -60,35 +60,6 @@ const DetailQuiz = ({ socket }) => {
 
   return (
     <MainLayout>
-      {/*
-            {(quiz.questions || []).map((qt) => (
-              <Card
-                key={qt._id}
-                title={qt.question}
-                size="small"
-                style={{ marginBottom: "10px" }}
-              >
-                <List
-                  itemLayout="horizontal"
-                  dataSource={qt.answers}
-                  renderItem={(answer, index) => (
-                    <List.Item>
-                      <List.Item.Meta
-                        avatar={
-                          qt.correctAnswer === index
-                            ? CorrectIcon
-                            : IncorretIcocn
-                        }
-                        title={<a href="https://ant.design">{answer}</a>}
-                      />
-                    </List.Item>
-                  )}
-                />
-              </Card>
-            ))}
-          </>
-        )}
-      </div> */}
       <Layout>
         <Layout>
           <Sider width={200} className="site-layout-background">
@@ -103,49 +74,56 @@ const DetailQuiz = ({ socket }) => {
                   style={{
                     width: "332px",
                     height: "218px",
-                    paddingTop: "20px",
                   }}
-                  src="https://dean2020.edu.vn/wp-content/uploads/2020/03/anh-cho-con.jpg"
+                  src={
+                    quiz.coverImage === ""
+                      ? "https://tintuckhanhhoa.com/uploads/no_image_available.jpg"
+                      : quiz.coverImage
+                  }
                 />
-                 <div className="title">
-                <h2>Tên Quizz: {quiz.name}</h2>
-              </div>
-              <div className="number-play">
-                <div className="content">
-                  <h4>{quiz.reports?.length} lượt</h4>
+                <div className="title">
+                  <h2>Tên: {quiz.name}</h2>
                 </div>
-                <div className="button-icon">
-                  <div>
-                    <FaPencilAlt />
+                <div className="number-play">
+                  <div className="content">
+                    <h4>{quiz.reports?.length} lượt</h4>
                   </div>
-                  <div>
-                    <FaEllipsisV />
+                  <div className="button-icon">
+                    <div>
+                      <FaPencilAlt />
+                    </div>
+                    <div>
+                      <FaEllipsisV />
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="button">
-                <div className="button1">
-                  <Button
-                    className="button-start"
-                    style={{ backgroundColor: "#416CDA" }}
-                  >
-                    <h3>Bắt đầu</h3>
-                  </Button>
+                <div className="button">
+                  <div className="button1">
+                    <Button
+                      className="button-start"
+                      style={{ backgroundColor: "#416CDA" }}
+                    >
+                      <Link
+                        to={`/host/start/${quiz._id}`}
+                        style={{ color: "#fff", fontSize: "14px" }}
+                      >
+                        Bắt đầu game
+                      </Link>
+                    </Button>
+                  </div>
+                </div>
+                <div className="description">
+                  <h5>
+                    Mô tả: <span>{quiz.description}</span>
+                  </h5>
+                </div>
+                <div className="user">
+                  <div>
+                    <FaUserAlt />
+                  </div>
+                  <div className="user-content">{user.email}</div>
                 </div>
               </div>
-              <div className="description">
-                <h5>
-                  Mô tả: <span>ccc</span>
-                </h5>
-              </div>
-              <div className="user">
-                <div>
-                  <FaUserAlt />
-                </div>
-                <div className="user-content">huyhtph07087@fpt.edu.vn</div>
-              </div>
-              </div>
-             
             </Menu>
           </Sider>
           <Layout style={{ padding: "0 24px 24px", paddingLeft: "150px" }}>
@@ -162,64 +140,66 @@ const DetailQuiz = ({ socket }) => {
                 minHeight: 1000,
               }}
             >
-              {(quiz.questions || []).map((qt,i) => (<div className="quizquestion">
-                <div className="quizzquestion-top">
-                  <div className="quizquestion-left">
-                    <h4>{i+1}-Câu hỏi</h4>
-                    <h5>{qt.question}</h5>
-                  </div>
-                  <div className="quizquestion-right">
-                    <Image
-                      style={{
-                        width: "234px",
-                        height: "119px",
-                        paddingTop: "5px",
-                        borderRadius: "15px",
-                        marginRight: "20px",
-                        display: "block",
-                      }}
-                      src={qt.image}
-                    ></Image>
-                    <Button
-                    style={{
-                      position: "absolute",
-                      backgroundColor: "#151414",
-                      color: "#fff",
-                      borderRadius: "5px",
-                      opacity: "70%",
-                      border: 0,
-                      marginTop: '85px',
-                      marginLeft: '-80px'
-                    }}
-                  >
-                    20s
-                  </Button>
-                  </div>
-                </div>
-               
-                <div className="quizzquestion-bottom">
-                {(qt.answers || []).map((as,index) => (
-                  <div className="answer">
-                  <div className="answer-question">
-                    <div className="answer-question-left">
-                      <h3>{QUESTION_LABELS[index]}</h3>
+              {(quiz.questions || []).map((qt, i) => (
+                <div className="quizquestion">
+                  <div className="quizzquestion-top">
+                    <div className="quizquestion-left">
+                      <h4>{i + 1}-Câu hỏi</h4>
+                      <h5>{qt.question}</h5>
                     </div>
-                    <div className="answer-question-right">
-                      <h4>{as}</h4>
+                    <div className="quizquestion-right">
+                      <Image
+                        style={{
+                          width: "234px",
+                          height: "119px",
+                          paddingTop: "5px",
+                          borderRadius: "15px",
+                          marginRight: "20px",
+                          display: "block",
+                        }}
+                        src={
+                          qt.image === ""
+                            ? "https://tintuckhanhhoa.com/uploads/no_image_available.jpg"
+                            : qt.image
+                        }
+                      ></Image>
+                      <Button
+                        style={{
+                          position: "absolute",
+                          backgroundColor: "#151414",
+                          color: "#fff",
+                          borderRadius: "5px",
+                          opacity: "70%",
+                          border: 0,
+                          marginTop: "85px",
+                          marginLeft: "-80px",
+                        }}
+                      >
+                        20s
+                      </Button>
                     </div>
                   </div>
-                  <div className="answer-icon">{
-                          qt.correctAnswer === index
+                  <div className="quizzquestion-bottom">
+                    {(qt.answers || []).map((as, index) => (
+                      <div className="answer">
+                        <div className="answer-question">
+                          <div className="answer-question-left">
+                            <h3>{QUESTION_LABELS[index]}</h3>
+                          </div>
+                          <div className="answer-question-right">
+                            <h4>{as}</h4>
+                          </div>
+                        </div>
+                        <div className="answer-icon">
+                          {qt.correctAnswer === index
                             ? CorrectIcon
-                            : IncorretIcocn
-                        }</div>
+                            : IncorretIcocn}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
                 </div>
-                ))}
-                </div>
-              </div>))}
-              
-              
-              
+              ))}
             </Content>
           </Layout>
         </Layout>
