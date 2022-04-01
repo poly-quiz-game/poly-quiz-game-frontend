1ac582d61593a75791d1317d8e13d670d23e3c2a
hieupv2412 <hieu.pham@desports.gg>
2022-04-01 19:19:36 +0700

    update ui report

--- src/features/report/pages/ReportDetailPlayers.jsx
+++ src/features/report/pages/ReportDetailPlayers.jsx
@@ -1,12 +1,12 @@
-import React, {useEffect, useState} from 'react'
-import {Menu, Row, Table, Input, Modal, Progress, Col} from 'antd'
-import {Link, useParams} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Menu, Row, Table, Input, Modal, Progress, Col } from 'antd'
+import { Link, useParams } from 'react-router-dom'
 import ReportDetail from './ReportDetail'
 import styled from 'styled-components'
 import reportApi from 'api/reportApi'
 import ReportPlayerEntities from './ReportPlayerEntities'
-import {ReactComponent as UserIcon} from '../../../assets/images/UserIcon.svg'
-import {formatNumber} from "../../../utils";
+import { ReactComponent as UserIcon } from '../../../assets/images/UserIcon.svg'
+import { formatNumber } from '../../../utils'
 
 const WrapSearch = styled.div`
   display: flex;
@@ -57,163 +57,171 @@ const StyledTable = styled((props) => <Table {...props} />)`
 `
 
 const columns = [
-    {
-        title: 'Tên',
-        dataIndex: 'name',
-        sorter: true,
-        // align: 'center',
-        render: (name) => `${name}`,
-        width: '40%',
-    },
-    {
-        title: 'Xếp hạng',
-        sorter: true,
-        align: 'right',
-        dataIndex: 'rank',
-        width: '15%',
-    },
-    {
-        title: 'Câu trả lời đúng',
-        sorter: true,
-        dataIndex: 'correctAnswersCount',
-        align: 'right',
-        render: (correctAnswersCount) => {
-            return (
-                <>
-                    <Row gutter={[4, 4]} justify='space-between'>
-                        <Col span={12}>
-                            <Progress type="circle" width='35px' strokeWidth='15' strokeColor="rgb(38, 137, 12)" percent={correctAnswersCount} format={() => ``}/>
-                        </Col>
-                        <Col span={12}>
-                            {formatNumber(correctAnswersCount)}
-                        </Col>
-                    </Row>
-                </>
-            )
-        },
-        width: '15%',
-    },
-    // {
-    //     title: 'Số câu sai',
-    //     sorter: true,
-    //     align: 'right',
-    //     dataIndex: 'unansweredCount',
-    //     render: (unansweredCount) => {
-    //         return (
-    //             <>
-    //                 <Row gutter={[4, 4]} justify='space-between'>
-    //                     <Col span={12}>
-    //                         <Progress type="circle" width='35px' strokeWidth='15' strokeColor="rgb(38, 137, 12)" percent={unansweredCount} format={() => ``}/>
-    //                     </Col>
-    //                     <Col span={12}>
-    //                         {formatNumber(unansweredCount)}
-    //                     </Col>
-    //                 </Row>
-    //             </>
-    //         )
-    //     },
-    //     width: '15%',
-    // },
-    {
-        title: 'Tổng điểm',
-        sorter: true,
-        align: 'right',
-        dataIndex: 'totalPoints',
-        width: '15%',
-    },
-]
-function correctAnswer (user) {
-    return user?.playerAnswers.reduce((acc, cur) => {
-        const result =  user.report.reportQuestions.filter(reportQ => reportQ.id === cur.questionId)
-        return result[0].correctAnswer === cur.answer ? acc + 1 : acc
-    }, 0)
-}
-const ReportDetailPlayers = ({report}) => {
-    const [players, setPlayers] = useState({
-        data: [],
-        loading: false,
-    })
-    const [limit, setLimit] = useState(10)
-    const {id} = useParams()
-
-    const [isModalVisible, setIsModalVisible] = useState(false)
-    const [player, setPlayer] = useState('')
-
-    const showModal = () => {
-        setIsModalVisible(true)
-    }
-
-    const handleOk = () => {
-        setIsModalVisible(false)
-    }
-
-    const handleCancel = () => {
-        setIsModalVisible(false)
-    }
-    const fetchData = async () => {
-        setPlayers((state) => ({
-            ...state,
-            loading: true
-        }))
-        const data = await reportApi.getPlayerInReport({id, offset: 0, limit: limit})
-        setPlayers({
-            data: data && data.length > 0 && data.map((user, index) => {
-                return {
-                    id: user.id,
-                    rank: index + 1,
-                    name: user.name,
-                    correctAnswersCount: correctAnswer(user) * 100 / user.playerAnswers.length ,
-                    // unansweredCount: (user.playerAnswers.length - correctAnswer(user))  * 100 / user.playerAnswers.length,
-                    totalPoints: user.score
-                }
-            }),
-            loading: false
-        })
-    }
-    useEffect(() => {
-        fetchData && fetchData()
-    }, [setPlayer])
-
-    return (
+  {
+    title: 'Tên',
+    dataIndex: 'name',
+    sorter: true,
+    // align: 'center',
+    render: (name) => `${name}`,
+    width: '40%',
+  },
+  {
+    title: 'Xếp hạng',
+    sorter: true,
+    align: 'right',
+    dataIndex: 'rank',
+    width: '15%',
+  },
+  {
+    title: 'Câu trả lời đúng',
+    sorter: true,
+    dataIndex: 'correctAnswersCount',
+    align: 'right',
+    render: (correctAnswersCount) => {
+      return (
         <>
-            <ReportDetail>
-                <TableWrapper>
-                    <Wrapper>
-                        <div style={{width: '100%'}}>
-                            <WrapSearch>
-                                <WrapInputSearch>
-                                    <DivInput>
-                                        <InputSearch placeholder='Search'/>
-                                    </DivInput>
-                                </WrapInputSearch>
-                            </WrapSearch>
-                        </div>
-                        <StyledTable
-                            columns={columns}
-                            onRow={(record, rowIndex) => {
-                                return {
-                                    onClick: (event) => {
-                                        setPlayer(record)
-                                        setIsModalVisible(true)
-                                    }, // click row
-                                }
-                            }}
-                            rowKey={record => record.id}
-                            dataSource={players?.data.length?players?.data : []}
-                            pagination={false}
-                            loading={players.loading}
-                            // onChange={this.handleTableChange}
-                        />
-                    </Wrapper>
-                </TableWrapper>
-                {isModalVisible && (
-                    <Modal title={player?.name} width={1080} visible={true} onOk={handleOk} onCancel={handleCancel}>
-                        <ReportPlayerEntities id={id} player={player}/>
-                    </Modal>
-                )}
-            </ReportDetail>
+          <Row gutter={[4, 4]} justify='space-between'>
+            <Col span={12}>
+              <Progress
+                type='circle'
+                width='35px'
+                strokeWidth='15'
+                strokeColor='rgb(38, 137, 12)'
+                percent={correctAnswersCount}
+                format={() => ``}
+              />
+            </Col>
+            <Col span={12}>{formatNumber(correctAnswersCount)}</Col>
+          </Row>
         </>
-    )
+      )
+    },
+    width: '15%',
+  },
+  // {
+  //     title: 'Số câu sai',
+  //     sorter: true,
+  //     align: 'right',
+  //     dataIndex: 'unansweredCount',
+  //     render: (unansweredCount) => {
+  //         return (
+  //             <>
+  //                 <Row gutter={[4, 4]} justify='space-between'>
+  //                     <Col span={12}>
+  //                         <Progress type="circle" width='35px' strokeWidth='15' strokeColor="rgb(38, 137, 12)" percent={unansweredCount} format={() => ``}/>
+  //                     </Col>
+  //                     <Col span={12}>
+  //                         {formatNumber(unansweredCount)}
+  //                     </Col>
+  //                 </Row>
+  //             </>
+  //         )
+  //     },
+  //     width: '15%',
+  // },
+  {
+    title: 'Tổng điểm',
+    sorter: true,
+    align: 'right',
+    dataIndex: 'totalPoints',
+    width: '15%',
+  },
+]
+function correctAnswer(user) {
+  return user?.playerAnswers.reduce((acc, cur) => {
+    const result = user.report.reportQuestions.filter((reportQ) => reportQ.id === cur.questionId)
+    return result[0].correctAnswer === cur.answer ? acc + 1 : acc
+  }, 0)
+}
+const ReportDetailPlayers = ({ report }) => {
+  const [players, setPlayers] = useState({
+    data: [],
+    loading: false,
+  })
+  const [limit, setLimit] = useState(10)
+  const { id } = useParams()
+
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const [player, setPlayer] = useState('')
+
+  const showModal = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleOk = () => {
+    setIsModalVisible(false)
+  }
+
+  const handleCancel = () => {
+    setIsModalVisible(false)
+  }
+  const fetchData = async () => {
+    setPlayers((state) => ({
+      ...state,
+      loading: true,
+    }))
+    const data = await reportApi.getPlayerInReport({ id, offset: 0, limit: limit })
+    setPlayers({
+      data:
+        data &&
+        data.length > 0 &&
+        data.map((user, index) => {
+          return {
+            id: user.id,
+            rank: index + 1,
+            name: user.name,
+            correctAnswersCount: (correctAnswer(user) * 100) / user.playerAnswers.length,
+            // unansweredCount: (user.playerAnswers.length - correctAnswer(user))  * 100 / user.playerAnswers.length,
+            totalPoints: user.score,
+          }
+        }),
+      loading: false,
+    })
+  }
+  useEffect(() => {
+    fetchData && fetchData()
+  }, [setPlayer])
+
+  return (
+    <>
+      <ReportDetail>
+        <TableWrapper>
+          <Wrapper>
+            <div style={{ width: '100%' }}>
+              <WrapSearch>
+                <WrapInputSearch>
+                  <DivInput>
+                    <InputSearch placeholder='Search' />
+                  </DivInput>
+                </WrapInputSearch>
+              </WrapSearch>
+            </div>
+            <StyledTable
+              columns={columns}
+              onRow={(record, rowIndex) => {
+                return {
+                  onClick: (event) => {
+                    setPlayer(record)
+                    setIsModalVisible(true)
+                  }, // click row
+                }
+              }}
+              rowKey={(record) => record.id}
+              dataSource={players?.data.length ? players?.data : []}
+              pagination={false}
+              loading={players.loading}
+              // onChange={this.handleTableChange}
+            />
+          </Wrapper>
+        </TableWrapper>
+        {isModalVisible && (
+          <Modal title={player?.name} width={1080} visible={true} onOk={handleOk} onCancel={handleCancel}>
+            <ReportPlayerEntities id={id} player={player} />
+          </Modal>
+        )}
+      </ReportDetail>
+    </>
+  )
 }
 
 export default ReportDetailPlayers
--- src/features/report/pages/ReportPlayerEntities.jsx
+++ src/features/report/pages/ReportPlayerEntities.jsx
@@ -1,10 +1,10 @@
-import React, {useEffect, useState} from 'react'
-import {Menu, Row, Table, Input, Modal} from 'antd'
-import {Link, useParams} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { Menu, Row, Table, Input, Modal, Col, Progress } from 'antd'
+import { Link, useParams } from 'react-router-dom'
 import ReportDetail from './ReportDetail'
 import styled from 'styled-components'
 import reportApi from 'api/reportApi'
-import {formatNumber, getTypeQuestion} from "../../../utils";
+import { formatNumber, getTypeQuestion } from '../../../utils'
 
 const Container = styled.div`
   -webkit-box-flex: 1;
@@ -54,101 +54,115 @@ const InfoRankUser = styled.div`
   flex-direction: column;
   margin: 2rem 0px;
 `
-const data = [
-    {id: 1, question: 'cau hoi 1', type: 1, Answered: 3, correctIncorrect: 1},
-]
+const data = [{ id: 1, question: 'cau hoi 1', type: 1, Answered: 3, correctIncorrect: 1 }]
 const columns = [
-    {
-        title: 'Câu hỏi',
-        dataIndex: 'blockTitle',
-        // sorter: true,
-        // render: (name) => `${name.first} ${name.last}`,
-        width: '40%',
-    },
-    {
-        title: 'Loại',
-        sorter: true,
-        dataIndex: 'type',
-        width: '15%',
-    },
-    {
-        title: 'Câu trả lời',
-        sorter: true,
-        dataIndex: 'displayText',
-        width: '15%',
-    },
-    {
-        title: 'Trạng thái',
-        sorter: true,
-        dataIndex: 'status',
-        width: '15%',
-    },
-    {
-        title: 'Thời gian',
-        sorter: true,
-        dataIndex: 'time',
-        width: '15%',
-    },
-    // {
-    //     title: 'Points',
-    //     sorter: true,
-    //     dataIndex: 'point',
-    //     width: '15%',
-    // },
+  {
+    title: 'Câu hỏi',
+    dataIndex: 'blockTitle',
+    // sorter: true,
+    // render: (name) => `${name.first} ${name.last}`,
+    width: '40%',
+  },
+  {
+    title: 'Loại',
+    sorter: true,
+    dataIndex: 'type',
+    width: '15%',
+  },
+  {
+    title: 'Câu trả lời',
+    sorter: true,
+    dataIndex: 'displayText',
+    width: '15%',
+  },
+  {
+    title: 'Trạng thái',
+    sorter: true,
+    dataIndex: 'status',
+    width: '15%',
+  },
+  {
+    title: 'Thời gian',
+    sorter: true,
+    dataIndex: 'time',
+    width: '15%',
+  },
+  // {
+  //     title: 'Points',
+  //     sorter: true,
+  //     dataIndex: 'point',
+  //     width: '15%',
+  // },
 ]
-const ReportPlayerEntities = ({id, player}) => {
-    const [playerAnswer, setPlayerAnswer] = useState([])
-    const [loading, setLoading] = useState(false)
-    const fetchData = async () => {
-        setLoading(true)
-        const data = await reportApi.getAllAnswerOfOnePlayer(id, player?.id)
-        setPlayerAnswer(data)
-        // console.log('data', data)
+const ReportPlayerEntities = ({ id, player }) => {
+  console.log('player', player)
+  const [playerAnswer, setPlayerAnswer] = useState([])
+  const [loading, setLoading] = useState(false)
+  const fetchData = async () => {
+    setLoading(true)
+    const data = await reportApi.getAllAnswerOfOnePlayer(id, player?.id)
+    setPlayerAnswer(data)
+    console.log('data', data)
 
-        setLoading(false)
-    }
-    useEffect(() => {
-        player?.id && fetchData && fetchData()
-        console.log('player', player)
+    setLoading(false)
+  }
+  useEffect(() => {
+    player?.id && fetchData && fetchData()
+    console.log('player', player)
+  }, [setPlayerAnswer])
+  return (
+    <>
+      <TableWrapper>
+        <Container>
+          <WrapRankUser>
+            <PercentageAnswererWrap>
+              <PercentageAnswerer>
+                {/* <span>
+                  {formatNumber(player.correctAnswersCount / (player.correctAnswersCount + player.unansweredCount))}{' '}
+                  đúng
+                </span> */}
+                <Row gutter={[4, 4]} justify='space-between'>
+                  <Col span={12}>
+                    <Progress
+                      type='circle'
+                      width='55px'
+                      strokeWidth='15'
+                      strokeColor='rgb(38, 137, 12)'
+                      percent={player?.correctAnswersCount}
+                      format={() => ``}
+                    />
+                  </Col>
+                  <Col span={12}>{formatNumber(player?.correctAnswersCount * 10)}</Col>
+                </Row>
+              </PercentageAnswerer>
+            </PercentageAnswererWrap>
+            <InfoRankUserWrap>
+              <InfoRankUser>Xếp hạng {player.rank}</InfoRankUser>
+              {/*<InfoRankUser>Questions answered</InfoRankUser>*/}
+            </InfoRankUserWrap>
+          </WrapRankUser>
 
-    }, [setPlayerAnswer]);
-    return (
-        <>
-            <TableWrapper>
-                <Container>
-                    <WrapRankUser>
-                        <PercentageAnswererWrap>
-                            <PercentageAnswerer>
-                                <span>{formatNumber(player.correctAnswersCount / (player.correctAnswersCount + player.unansweredCount))} đúng</span>
-                            </PercentageAnswerer>
-                        </PercentageAnswererWrap>
-                        <InfoRankUserWrap>
-                            <InfoRankUser>Xếp hạng {player.rank}</InfoRankUser>
-                            {/*<InfoRankUser>Questions answered</InfoRankUser>*/}
-                        </InfoRankUserWrap>
-                    </WrapRankUser>
-
-                    <Wrapper>
-                        <StyledTable
-                            columns={columns}
-                            onRow={(record, rowIndex) => {
-                                return {
-                                    onClick: (event) => {
-                                        // setIsModalVisible(true)
-                                    }, // click row
-                                }
-                            }}
-                            rowKey={record => record.id}
-                            dataSource={playerAnswer}
-                            pagination={false}
-                            loading={loading}
-                            // onChange={this.handleTableChange}
-                        />
-                    </Wrapper>
-                </Container>
-            </TableWrapper>
-        </>
-    )
+          <Wrapper>
+            <StyledTable
+              columns={columns}
+              onRow={(record, rowIndex) => {
+                return {
+                  onClick: (event) => {
+                    // setIsModalVisible(true)
+                  }, // click row
+                }
+              }}
+              rowKey={(record) => record.id}
+              dataSource={playerAnswer}
+              pagination={false}
+              loading={loading}
+              // onChange={this.handleTableChange}
+            />
+          </Wrapper>
+        </Container>
+      </TableWrapper>
+    </>
+  )
 }
 
 export default ReportPlayerEntities
